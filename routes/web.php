4c5f060637688ede5965c3b7eba7d4029c5740c6
Oliver Servín <oliverds@users.noreply.github.com>
2023-05-06 14:52:54 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Spatie\RouteDiscovery\Discovery\Discover;
 */
 
 Discover::views()->in(resource_path('views/discovery'));
-Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
+// Discover::controllers()->in(app_path('Http/Controllers/Discovery'));
